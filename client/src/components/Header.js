3c2cef21531e9e0f1fa4226fb3aa20cad672d873
bcoelho <bruneves2002@yahoo.com.br>
2024-05-29 16:38:50 -0300

    new header01

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,8 +10,8 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "red" : "blue"}
-          text={showAdd ? "Exit" : "Add"}
+          color={showAdd ? "blue" : "black"}
+          text={showAdd ? "Sair" : "Adicionar"}
           onClick={onAdd}
         />
       )}
